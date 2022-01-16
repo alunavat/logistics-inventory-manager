c5f9b0d8db76eec138414f00e897bea08a921a58
Anmol Lunavat <44456886+alunavat@users.noreply.github.com>
2022-01-16 12:10:34 -0500

    First Commit
    Added basic files for the first commit.

---
+++ src/main/java/com/shopify/logistics/logisticsinventorymanager/LogisticsInventoryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.shopify.logistics.logisticsinventorymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsInventoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsInventoryManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopify/logistics/logisticsinventorymanager/LogisticsInventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopify.logistics.logisticsinventorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsInventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
